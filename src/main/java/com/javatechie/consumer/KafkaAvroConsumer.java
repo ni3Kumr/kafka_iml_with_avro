b359e5082e40fead7d5c0f72f92ed0f936289596
Nitin Kumar <121708351+ni3Kumr@users.noreply.github.com>
2026-02-14 18:22:09 +0530

    Sendind data from csv file to kafka producer

--- src/main/java/com/javatechie/consumer/KafkaAvroConsumer.java
+++ src/main/java/com/javatechie/consumer/KafkaAvroConsumer.java
@@ -1,10 +1,14 @@
 package com.javatechie.consumer;
 
 import com.javatechie.dto.Employee;
+import org.springframework.kafka.annotation.DltHandler;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.messaging.handler.annotation.Header;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,11 +17,25 @@ public class KafkaAvroConsumer {
     private static final Logger log =
             LoggerFactory.getLogger(KafkaAvroConsumer.class);
 
+    @RetryableTopic(attempts = "4")
     @KafkaListener(topics = "${topic.name}")
-    public void read(ConsumerRecord<String, Employee> consumerRecord) {
+    public void read(ConsumerRecord<String, Employee> consumerRecord, @Header(KafkaHeaders.RECEIVED_TOPIC)String topic,@Header(KafkaHeaders.OFFSET)long offset) {
         String key = consumerRecord.key();
         Employee employee = consumerRecord.value();
-        log.info("Avro message received for key : {} value : {}", key, employee.toString());
+
+        if(employee.getAge() <21){
+            throw  new RuntimeException("You are not authorise to work here");
+
+        }
+        log.info("Avro message received for key : {} value : {}  topic : {} offset:{}", key, employee.toString(),topic,offset);
 
     }
+
+    @DltHandler
+    public void listenerDTL(ConsumerRecord<String, Employee> consumerRecord, @Header(KafkaHeaders.RECEIVED_TOPIC)String topic,@Header(KafkaHeaders.OFFSET)long offset){
+
+        log.info("DLT received :{} from : {} offset:{}",consumerRecord.value().getFirstName(),topic,offset);
+    }
+
+
 }
--- src/main/java/com/javatechie/controller/EventController.java
+++ src/main/java/com/javatechie/controller/EventController.java
@@ -2,10 +2,13 @@ package com.javatechie.controller;
 
 import com.javatechie.dto.Employee;
 import com.javatechie.producer.KafkaAvroProducer;
+import com.javatechie.util.CsvUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class EventController {
@@ -15,7 +18,10 @@ public class EventController {
     @PostMapping("/events")
     public String sendMessage(@RequestBody Employee employee) {
 
-            producer.send(employee);
+        List<Employee> employees = CsvUtil.readEmployeesFromClasspath();
+
+        employees.forEach( emp ->  producer.send(emp));
+          //  producer.send(employee);
 
 
         return "message published !";
